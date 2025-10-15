c8fe98c59a7437abe39e77af7cc0121a9e53c235
varunbalanagu <147472935+varunbalanagu@users.noreply.github.com>
2025-10-15 21:47:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Peak element/peak-element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int peakElement(vector<int> &arr) {
+        int n=arr.size();
+        int low=0,high=n-1;
+        while(low<high)
+        {
+            int mid=(low+high)/2;
+            if(arr[mid]<arr[mid+1])
+            {
+                low=mid+1;
+            }
+            else
+            {
+                high=mid;
+            }
+        }
+        return low;
+    }
+};
